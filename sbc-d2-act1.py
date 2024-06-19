2bfdc5bc2fb5f8f1bdcc0ba5e1559eec05ce62ec
Diana24-Gonia <fielyan2@gmail.com>
2024-06-18 22:13:07 -0700

    Add files via upload

---
+++ sbc-d2-act1.py
@@ -0,0 +1,19 @@
+#Summer Bootcamp
+#Summer bootcamP
+#Loot
+#camE
+#AR
+#True = Alpha
+
+SBC = "summer bootcamp"
+print(SBC.title())
+
+print(f"{SBC[0:14].capitalize() + SBC[-1].capitalize()}")
+
+print(SBC[7:11].replace("b","L"))
+
+print(SBC[11:15].replace("p","E"))
+
+print(SBC[-3].upper()+ SBC[5].upper())
+
+print(SBC.replace(" ", "he") .isalpha())
