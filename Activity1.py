90e28a3b6529c47231c34bab11b8dc684db98225
unknown <fielyan2@gmail.com>
2024-06-18 22:44:21 -0700

    version1

---
+++ Activity1.py
@@ -0,0 +1,21 @@
+print ('spam eggs')
+print ("Paris rabbit got your back :)! Yay!") 
+print ('1975')
+print('doesn\'t')
+print("doesn't")
+print('"No way i am  innocent," i said.')
+print("Yes, they said.")
+print('First line.\nSecond line.')
+print('i love me. i love to eat.')
+print('C:\gikabuhi\ndiana')
+print(r'C:\fiel\carlito')
+#hello this is our activity for our bootcamp
+print("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+print (2 * 'cry' + ' ka nalang')
+
+#list
+print ("squares = [1, 4, 9, 16, 25]")
